980d69389e54d57b8af210388e6f46140c3ad6a1
Arkadiusz Niedzielski <arknie08576@wp.pl>
2026-03-25 17:47:35 +0100

    first commit, empty project

---
+++ src/main/java/pl/telco/incident/TelcoIncidentManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.telco.incident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoIncidentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelcoIncidentManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/telco/incident/TelcoIncidentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.telco.incident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoIncidentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
